6e53cf4975bebd7bbc2a99a9358adb94f180d891
Zineb Senane <senane.zineb.mpsi@gmail.com>
2023-05-18 18:49:45 +0200

    data_loaders for word and char levels

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -6,28 +6,26 @@ from gensim.models import Word2Vec, KeyedVectors
 import gensim.downloader as api
 from tqdm import tqdm
 
-class CornellMovieDialogsLoader:
+class ShakespearePlaysLoader:
     def __init__(self, dataset_dir, level='word'):
         self.dataset_dir = dataset_dir
-        self.word2vec_path = './data/embeddings/word2vec.wordvectors'
-        self.wv = KeyedVectors.load(self.word2vec_path, mmap='r')
-        # self.wv = api.load('word2vec-google-news-300')
-        # self.wv = KeyedVectors.load_word2vec_format('
-        self.embedding_dim = self.wv.vector_size
+        if level == 'word':
+          self.word2vec_path = './data/embeddings/word2vec.wordvectors'
+          self.wv = KeyedVectors.load(self.word2vec_path, mmap='r')
+          # self.wv = api.load('word2vec-google-news-300')
+          # self.wv = KeyedVectors.load_word2vec_format('
+          self.embedding_dim = self.wv.vector_size
         self.level = level
         self.vocab_size = None
         self.mappings_dir = './data/mappings'
+        self.plays_data= self.read_Plays()
 
-    def read_dialogue_lines(self):
-        print('Reading dialogue lines...')
-        dialogue_lines = {}
-        with open(os.path.join(self.dataset_dir, 'movie_lines.txt'), 'r', encoding='iso-8859-1') as f:
-            for row in f:
-                line=row.strip().split(" +++$+++ ")
-                if len(line) == 5 and line[4]:
-                    dialogue_lines[line[0]] = line[4].strip()
+    def read_Plays(self):
+        print('Reading Shakespeare Plays...')
+        with open("./data/ShakespearePlays/shakespeare.txt", 'r') as f:
+            plays_data = f.read()  
                
-        return dialogue_lines
+        return plays_data
     
     def build_word_vocab(self):
         print('Building word vocabulary...')
@@ -52,23 +50,39 @@ class CornellMovieDialogsLoader:
         self.vocab_size = len(self.word_to_id)
 
         return self.word_to_id, self.id_to_word
+    
+    def build_char_vocab(self):
+        print('Building character vocabulary...')
+        # check if the mapping folder is empty, then save the mappings, else load the mappings
+        if not os.listdir('./data/mappings'):
+            unique_chars = sorted(set(self.plays_data))
+            self.char_to_id = {char: idx for idx, char in enumerate(unique_chars)}
+            self.id_to_char = {idx: char for idx, char in enumerate(unique_chars)}
+            self.char_to_id['<UNK>'] = len(unique_chars)
+            self.id_to_char[len(unique_chars)] = '<UNK>'
+            self.save_mappings(self.mappings_dir, self.char_to_id, self.id_to_char)
+        else:
+            self.char_to_id, self.id_to_char = self.load_mappings(self.mappings_dir)
+            
+        self.vocab_size = len(self.char_to_id)
+        return self.char_to_id, self.id_to_char
+            
+        
 
-    def text_to_ids(self, dialog_lines, word_to_id):
-        ids = [[word_to_id.get(word, word_to_id['<UNK>']) for word in line] for line in dialog_lines.values()]
-        # text_ids = [word_to_id.get(word, word_to_id['<UNK>']) for word in text]
+    def text_to_ids_word(self, plays, word_to_id):
+        ids = [word_to_id.get(word, word_to_id['<UNK>']) for word in plays.split()]
         return ids
     
-    def text_to_vectors(self, text):
-        print('Converting text to vectors...')
-        vectors = [self.wv[word] if word in self.wv else self.wv['<UNK>'] for word in text]
-        return vectors
+    def text_to_ids_char(self, plays, char_to_id):
+        ids = [char_to_id.get(char, char_to_id['<UNK>']) for char in plays]
+        return ids
 
-    def preprocess(self):
+    
+
+    def preprocess_word_level(self):
         print('Preprocessing data...')
-        dialogue_lines = self.read_dialogue_lines()
-
         word_to_id, id_to_word = self.build_word_vocab()
-        text_id = self.text_to_ids(dialogue_lines, word_to_id)
+        text_id = self.text_to_ids_word(self.plays_data, word_to_id)
 
         # Create embedding matrix
         embedding_matrix = torch.zeros((len(word_to_id), self.embedding_dim))
@@ -79,18 +93,21 @@ class CornellMovieDialogsLoader:
         
         return text_id, word_to_id, id_to_word, embedding_matrix, self.vocab_size
     
+    def preprocess_char_level(self):
+        print('Preprocessing data...')
+        char_to_id, id_to_char = self.build_char_vocab()
+        text_id = self.text_to_ids_char(self.plays_data, char_to_id)
+        return text_id, char_to_id, id_to_char, self.vocab_size
+    
     def create_dataset(self, tokenized_text, seq_length=50):
         print('Creating dataset...')
         input_sequences = []
         target_sequences = []
 
-        # Flatten the list of lists
-        flattened_text = [word for sentence in tokenized_text for word in sentence]
-
         # Create input-target pairs with the specified sequence length
-        for i in tqdm(range(0, len(flattened_text) - seq_length, 1)):
-            input_seq = flattened_text[i:i + seq_length]
-            target_seq = flattened_text[i + 1:i + seq_length + 1]
+        for i in tqdm(range(0, len(tokenized_text) - seq_length, 1)):
+            input_seq = tokenized_text[i:i + seq_length]
+            target_seq = tokenized_text[i + 1:i + seq_length + 1]
             input_sequences.append(input_seq)
             target_sequences.append(target_seq)
 
@@ -101,7 +118,7 @@ class CornellMovieDialogsLoader:
 
     
     @staticmethod
-    def create_loaders(dataset, train_split, val_split, test_split, batch_size):
+    def create_loaders(dataset, train_split, val_split, batch_size):
       train_size = int(train_split * len(dataset))
       val_size = int(val_split * len(dataset))
       test_size = len(dataset) - train_size - val_size  # this accounts for any remainder
@@ -111,18 +128,18 @@ class CornellMovieDialogsLoader:
       test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
       return train_loader, val_loader, test_loader
     
-    def save_mappings(self, save_dir, word_to_id, id_to_word):
+    def save_mappings(self, save_dir, _to_id, id_to_):
         with open(os.path.join(save_dir, f'{self.level}_to_id.json'), 'w') as f:
-            json.dump(word_to_id, f)
+            json.dump(_to_id, f)
 
         with open(os.path.join(save_dir, f'id_to_{self.level}.json'), 'w') as f:
-            json.dump(id_to_word, f)
+            json.dump(id_to_, f)
     
     def load_mappings(self, load_dir):
         with open(os.path.join(load_dir, f'{self.level}_to_id.json'), 'r') as f:
-            word_to_id = json.load(f)
+            _to_id = json.load(f)
 
         with open(os.path.join(load_dir, f'id_to_{self.level}.json'), 'r') as f:
-            id_to_word = json.load(f)
+            id_to_ = json.load(f)
 
-        return word_to_id, id_to_word
+        return _to_id, id_to_
