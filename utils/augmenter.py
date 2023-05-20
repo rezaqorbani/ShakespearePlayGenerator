69ab3338b86399a3b0c76a763ac6098d7ba1a9e7
Zineb Senane <senane.zineb.mpsi@gmail.com>
2023-05-20 17:22:18 +0200

    add augmentation.py and augmented text

--- utils/augmenter.py
+++ utils/augmenter.py
@@ -1,5 +1,6 @@
 import nlpaug.augmenter.word as naw
 from tqdm import tqdm
+from data_loader.CornellMovieLoader import CornellMovieLoader
 
 
 class TextAugmenter:
@@ -23,3 +24,26 @@ class TextAugmenter:
 
     def augment_word2vec(self, text):
         return [self.word2vec_aug.augment(line) for line in text]
+    
+    def augment(self, data_loader, filename):
+        passages, characters=data_loader.extract_passages()
+        characters=[character[0]+'\n' if character!=[] else '\n\n' for character in characters ]
+        N=6000
+        for i in range(N):
+            text_context = self.augment_context(passages[i*500:(i+1)*500])
+            text = '\n\n'.join([character+elt[0] for character, elt in zip(characters[i*500:(i+1)*500],text_context)])
+            # Write the text to a file
+            if i==0:
+                with open(filename, 'w') as f:
+                    f.write(text)
+            else:
+                with open(filename, 'a') as f:
+                    f.write('\n\n'+text)
+                    
+if __name__ == '__main__':
+    dataset_dir= './data/ShakespearePlays'
+    p=0.8
+    textAugmenter = TextAugmenter(augmentation_percentage=p)
+    data_loader = CornellMovieLoader(dataset_dir)
+    
+    textAugmenter.augment(data_loader, './data/ShakespearePlays/augmentation/augmented_text.txt')
